f20d4256a4ce7a17a73b420b3278a45031513c54
Benjamin David Walcott <benjamin.walcott@gmail.com>
2016-01-15 17:15:29 -0500

    Updated to use GridBagSizer.  wx.grid.Grid component now operational.  Cancel button added.

--- InteractiveROSETTA/scripts/constraints.py
+++ InteractiveROSETTA/scripts/constraints.py
@@ -22,41 +22,53 @@ class ConstraintPanel(wx.lib.scrolledpanel.ScrolledPanel):
       self.minPanel = minPanel
       print 'Panel initialized'
       #sizer
-      vbox = wx.BoxSizer(wx.VERTICAL)
-      hbox = wx.BoxSizer(wx.HORIZONTAL)
+      sizer = wx.GridBagSizer(10,10)
+      self.SetSizer(sizer)
+
       #Add constraint button
-      self.ConstraintBtn = wx.Button(self,-1,label="Add Constraint",pos=(10,10),size=(120,25))
+      self.ConstraintBtn = wx.Button(self,-1,label="Add Constraint",size=(120,25))
       self.ConstraintBtn.SetForegroundColour("#000000")
       self.ConstraintBtn.SetFont(wx.Font(10,wx.DEFAULT,wx.ITALIC,wx.BOLD))
       self.ConstraintBtn.Bind(wx.EVT_BUTTON,self.addConstraint)
       self.ConstraintBtn.SetToolTipString("Add a new constraint to the simulation")
-      hbox.Add(self.ConstraintBtn,1)
+      self.Sizer.Add(self.ConstraintBtn,(0,0),(1,1))
+      self.Layout()
       print 'constraint button done'
       #Save Constraints button
-      self.SaveConstraintsBtn = wx.Button(self,-1,label="Save Constraints",pos=(90,10),size=(120,25))
+      self.SaveConstraintsBtn = wx.Button(self,-1,label="Save Constraints",size=(120,25))
       self.SaveConstraintsBtn.SetForegroundColour("#000000")
       self.SaveConstraintsBtn.SetFont(wx.Font(10,wx.DEFAULT,wx.ITALIC,wx.BOLD))
       self.SaveConstraintsBtn.Bind(wx.EVT_BUTTON,self.saveConstraints)
       self.SaveConstraintsBtn.SetToolTipString("Save current constraints to a file for use in later simulations")
-      hbox.Add(self.SaveConstraintsBtn,1)
+      self.Sizer.Add(self.SaveConstraintsBtn,(0,1),(1,1))
+      self.Layout()
       print 'save button'
       #Load Constraints file button
-      self.LoadConstraintsBtn = wx.Button(self,-1,label="Load Constraints",pos=(180,10),size=(120,25))
+      self.LoadConstraintsBtn = wx.Button(self,-1,label="Load Constraints",size=(120,25))
       self.LoadConstraintsBtn.SetForegroundColour("#000000")
       self.LoadConstraintsBtn.SetFont(wx.Font(10,wx.DEFAULT,wx.ITALIC,wx.BOLD))
       self.LoadConstraintsBtn.Bind(wx.EVT_BUTTON,self.loadConstraints)
       self.LoadConstraintsBtn.SetToolTipString("Load constraints from a file")
-      hbox.Add(self.LoadConstraintsBtn,1)
+      self.Sizer.Add(self.LoadConstraintsBtn,(0,2),(1,1))
+      self.Layout()
       print 'load button'
+      self.Cancelables = []
 
       #Remove Constraint Button
 
       #Clear Constraints Button
 
-      vbox.Add(hbox)
 
-      self.vbox=vbox
-      self.SetSizer(vbox)
+      #Constraints Grid
+      print 'starting constraints grid'
+      self.constraintsGrid = wx.grid.Grid(self,size=(1000,500))
+      self.constraintsGrid.CreateGrid(0,6)
+      self.constraintsGrid.SetLabelFont(wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.BOLD))
+      self.Sizer.Add(self.constraintsGrid,(4,0),(1,3))
+      self.Layout()
+      print 'grid created'
+
+      #Scrolling
       self.SetupScrolling()
       print 'scrolling'
 
@@ -91,42 +103,70 @@ class ConstraintPanel(wx.lib.scrolledpanel.ScrolledPanel):
     def addConstraint(self,event):
       '''Adds a new constraint to the set of constraints'''
       logInfo('Add Constraint button clicked!')
-      self.constraintTypeText=wx.StaticText(self,-1,'Constraint Type',(10,25),(100,25))
+      self.constraintTypeText=wx.StaticText(self,-1,'Constraint Type',size=(100,25))
       self.constraintTypeText.SetFont(wx.Font(10,wx.DEFAULT,wx.ITALIC,wx.BOLD))
-      self.vbox.Add(self.constraintTypeText)
-      constraintTypes = ['AtomPair','Angle','Dihedral','Coordinate']
-      self.constraintTypeMenu = wx.ComboBox(self,pos=(120,20),size=(125,25),choices=constraintTypes,style=wx.CB_READONLY)
+      self.Sizer.Add(self.constraintTypeText,(1,0),(1,1))
+      self.Cancelables.append(self.constraintTypeText)
+      self.Layout()
+      constraintTypes = ['Choose Type','AtomPair','Angle','Dihedral','Coordinate']
+      self.constraintTypeMenu = wx.ComboBox(self,size=(125,25),choices=constraintTypes,style=wx.CB_READONLY)
       self.constraintTypeMenu.Bind(wx.EVT_COMBOBOX,self.setConstraintType)
-      self.vbox.Add(self.constraintTypeMenu)
+      self.Sizer.Add(self.constraintTypeMenu,(1,1),(1,1))
+      self.Cancelables.append(self.constraintTypeMenu)
+      self.Layout()
+      #Cancel Button
+      self.CancelBtn = wx.Button(self,-1,label='Cancel')
+      self.CancelBtn.Bind(wx.EVT_BUTTON,self.cancel)
+      self.Sizer.Add(self.CancelBtn,(1,2),(1,1))
+      self.Cancelables.append(self.CancelBtn)
+      self.Layout()
+
+    def cancel(self,event):
+      logInfo('Cancel Button Pressed!')
+      print('Cancel Button Pressed!')
+      for item in self.Cancelables:
+        item.Show(False)
+        item.Destroy()
+      self.Cancelables = []
+      self.Layout()
 
     def setConstraintType(self,event):
       logInfo('Constraint type selected!')
+      if self.constraintTypeMenu.GetStringSelection() == 'Choose Type':
+        event.skip()
       try:
-        pdbs = []
+        pdbs = ['Choose PDB']
         print pdbs
         for [indx, r, seqpos, poseindx, chainoffset, minType] in self.minPanel.minmap:
           print "poseindx",poseindx
           if len(pdbs) == 0:
             print 'appending',poseindx
             pdbs.append(poseindx)
+          isThere = False
           for i in range(0,len(pdbs)):
             if poseindx == pdbs[i]:
               print("%i = %i"%(poseindx,pdbs[i]))
+              isThere = True
               break
+          if not isThere:
             print 'appending',poseindx
             pdbs.append(poseindx)
         print pdbs
-        for i in range(0,len(pdbs)):
-          pdbs[i] = self.seqWin.poses[pdbs[i]].get_id()
-          print pdbds[i]
+        for i in range(1,len(pdbs)):
+          pdbs[i] = str(self.seqWin.poses[pdbs[i]].get_id())
+          print pdbs[i]
         print pdbs
-        self.PdbText = wx.StaticText(self,-1,'PDB',(10,60),(50,25))
+        self.PdbText = wx.StaticText(self,-1,'PDB',size=(50,25))
         self.PdbText.SetFont(wx.Font(10,wx.DEFAULT,wx.ITALIC,wx.BOLD))
-        self.vbox.Add(self.PdbText)
+        self.Cancelables.append(self.PdbText)
+        self.Sizer.Add(self.PdbText,(2,0),(1,1))
+        self.Layout()
         print 'pdbtext'
-        self.PdbMenu = wx.ComboBox(self,-1,(70,55),(125,25),choices=pdbs,style=wx.CB_READONLY)
+        self.PdbMenu = wx.ComboBox(self,size=(125,25),choices=pdbs,style=wx.CB_READONLY)
         self.PdbMenu.Bind(wx.EVT_COMBOBOX,self.setConstraintPDB)
-        self.vbox.Add(self.PdbMenu)
+        self.Cancelables.append(self.PdbMenu)
+        self.Sizer.Add(self.PdbMenu,(2,1),(1,1))
+        self.Layout()
         print 'pdbmenu'
       except Exception as e:
         print(e.message)
@@ -136,26 +176,3 @@ class ConstraintPanel(wx.lib.scrolledpanel.ScrolledPanel):
 
     def gridClick(self,event):
       logInfo('Constraints Grid Clicked!')
-
-#Figure out what wx Class this should inherit from
-#wait until you finish writing the panel class to work on this....
-class ConstraintPopUp():
-    '''This class will be the pop-up menu where the user actually selects the
-    constraints to add'''
-
-    def __init__(self,parent,pos,W,H):
-      logInfo('Initializing Contraint Pop-up!')
-      #inherited class __init__
-      #custom options
-
-    def setSeqWin(self,seqWin):
-        self.seqWin = seqWin
-
-    def setPyMOL(self, pymol):
-        self.pymol = pymol
-        self.cmd = pymol.cmd
-        self.stored = pymol.stored
-
-    def setSelectWin(self, selectWin):
-        self.selectWin = selectWin
-        self.selectWin.setProtPanel(self)
