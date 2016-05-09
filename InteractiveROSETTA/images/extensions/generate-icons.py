01500db05f1302129f9287c71aa0bb8430351ec5
Benjamin David Walcott <benjamin.walcott@gmail.com>
2016-05-09 13:53:48 -0400

    Added missing image files

--- InteractiveROSETTA/images/extensions/generate-icons.py
+++ InteractiveROSETTA/images/extensions/generate-icons.py
@@ -5,7 +5,7 @@ iconsizes = [8, 16, 22, 24, 32, 48, 64, 96, 256]
 for PNG in glob.glob("*.png"):
     # Ignore resized icons
     if ("-" in PNG):
-	continue
+        continue
     for size in iconsizes:
-	commandline = "convert " + PNG + " -resize " + str(size) + "x" + str(size) + " " + PNG.split(".png")[0] + "-" + str(size) + ".png"
-	res, output = commands.getstatusoutput(commandline)
+        commandline = "convert " + PNG + " -resize " + str(size) + "x" + str(size) + " " + PNG.split(".png")[0] + "-" + str(size) + ".png"
+        res, output = commands.getstatusoutput(commandline)
