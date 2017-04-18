d3e6ecc7c460c48d233fef0e17285678bf98653a
Benjamin David Walcott <benjamin.walcott@gmail.com>
2017-04-18 16:10:54 -0400

    Added relink.py script to properly link to libraries located in lib directory

---
+++ InteractiveROSETTA/scripts/relink.py
@@ -0,0 +1,30 @@
+from commands import getstatusoutput as run
+import glob
+
+
+def link_lib(lib_file,libs):
+    link_libs = [line.split()[0].strip() for line in run('otool -L %s'%(lib_file))[1].split('\n')]
+    link_libs.pop(0)
+    print link_libs
+
+    lib_libs = {}
+    for lib in libs:
+        for llib in link_libs:
+            if lib in llib:
+                lib_libs[lib] = llib
+                break
+    print lib_libs
+
+    for lib in sorted(lib_libs.keys()):
+        print lib
+        cmd = 'install_name_tool -change %s `pwd`/../lib/%s %s'%(lib_libs[lib], lib,lib_file)
+        print cmd
+        print run(cmd)
+    print run('otool -L %s'%(lib_file))[1]
+
+#relink libraries in lib directory
+
+libs = ['libGLEW.1.11.0.dylib','libfreetype.6.dylib','libgcc_s.1.dylib','libgfortran.3.dylib','libgfortran.dylib','libopenbabel.4.dylib','libpng16.16.dylib','libquadmath.0.dylib']
+link_lib("hmmstr4py_darwin.so",libs)
+other_libs = glob.glob("../lib/*.dylib")
+for lib in other_libs: link_lib(lib,libs)
